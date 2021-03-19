13f405d430681ab50f591a755262610b7ec265f7
kerelius <kerelius@email.com>
2021-03-19 20:07:25 +0300

    Full Project Structure updated

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // Importing dependencies
-const express = require('express')
-const logger = require('morgan')
-const bodyParser = require('body-parser')
+'use strict';
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+//const bodyParser = require('body-parser');
+const config = require('./config');
+const admin = require('./admin');
+
+// Adding Routes
+const signUp = require('./routes/signUp');
+const login = require('./routes/login');
+
+// Defining the port
+var port = process.env.PORT
 
 // instantiating the app
 const app = express()
-
-// Defining the port
-var port = process.env.PORT || 3000
 
 // Setting the view engine
 app.set('view engine', 'ejs')
@@ -17,31 +25,24 @@ app.engine("html", require("ejs").renderFile)
 app.use(express.static('views'));
 
 // Giving the server access to user input
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 // for logging everything in the terminal
 app.use(logger('dev'))
+
+// Using routes
+app.use('/signUp', signUp);
+app.use('/login', login);
 
 // Defining the routes
 app.get('/', (req, res) => {
     res.render('index.html')
 })
 
-app.get('/index.html', (req, res) => {
-    res.render('index.html')
-})
-
-app.get('/login.html', (req, res) => {
-    res.render('login.html')
-})
-
-app.get('/signUp.html', (req, res) => {
-    res.render('signUp.html')
-})
-
 // Making the app listinig for any request
 app.listen(port, function(){
-    console.log('App is running on port ' + port)
+    console.log('App is running http://localhost:' + port)
 })
 
